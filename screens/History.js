2d410c5e0155b9b00f5d67f356ac548cc742949a
nhut_pham <nhut.pham@ilogic.vn>
2019-05-26 17:04:48 +0700

    add history tab navigation

--- screens/History.js
+++ screens/History.js
@@ -1,36 +1,110 @@
-import React, { Component } from "react";
+import React from "react";
+import { Button, View, Text } from "react-native";
 import {
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    TextInput,
-    Platform,
-    StatusBar,
-    ScrollView,
-    Image,
-    Dimensions
-} from "react-native";
+  createBottomTabNavigator,
+  createAppContainer,
+  createMaterialTopTabNavigator
+} from 'react-navigation';
 
-export default class App extends Component {
-  
+import Icon from "react-native-vector-icons/Ionicons";
+
+class Booking extends React.Component {
   render() {
+    // dummy data start
+    const data = [];
+    const bookingDataTemp = {
+      description: "Bacon ipsum dolor amet",
+    };
+    for(var i=0;i<25; i++) {
+      const bookingData = { ...bookingDataTemp };
+      bookingData['id']=i;
+      data.push(bookingData); 
+    }
+    // dummy data end
+    
     return (
-      <View>
-        <Text>History Screen</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Booking Screen</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 50,
-    marginBottom: 20,
-    backgroundColor: "#fefffe"
+class Order extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>Order Screen</Text>
+        </View>
+      );
+    }
+}
+
+class SameDay extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Text>No bookings yet.</Text>
+          <Text>Make one today!</Text>
+        </View>
+      );
+    }
+}
+
+const RootStack = createMaterialTopTabNavigator(
+  {
+    Booking: {
+      screen: Booking,
+      navigationOptions: {
+        title: "Bookings",
+        tabBarIcon: ({ horizontal, tintColor }) => <Icon name="md-home" size={25} color={tintColor} />
+      }
+    },
+    Order: {
+      screen: Order,
+      navigationOptions: {
+        title: "Orders",
+        tabBarIcon: ({ horizontal, tintColor }) => <Icon name="md-book" size={25} color={tintColor} />
+      }
+    },
+    SameDay: {
+      screen: SameDay,
+      navigationOptions: {
+        title: "Same Day",
+        tabBarIcon: ({ horizontal, tintColor }) => <Icon name="md-settings" size={25} color={tintColor} />
+      }
+    },
   },
-  home: {
-    paddingHorizontal: 20
+  {
+    swipeEnabled: false,
+    lazy: true,
+    animationEnabled: false,
+    tabBarOptions: {
+      activeTintColor: '#00b14f',
+      inactiveTintColor: '#676767',
+      upperCaseLabel: false,
+      labelStyle: {
+        fontSize: 15,
+        fontWeight: 'bold',
+      },
+      style: {
+        backgroundColor: 'white',
+        // paddingTop: 20
+      },
+      indicatorStyle : {
+        backgroundColor: '#00b14f',
+        height: 4
+      }
+    }
   }
-});
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+export default App;
