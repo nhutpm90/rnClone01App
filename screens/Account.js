1f2c7dbfb56b910024e8ebee5bd95f8157592d62
nhut_pham <nhut.pham@ilogic.vn>
2019-05-27 10:21:27 +0700

    add account screen

--- screens/Account.js
+++ screens/Account.js
@@ -1,36 +1,99 @@
 import React, { Component } from "react";
-import { 
-    View,
-    Text,
-    StyleSheet,
-    SafeAreaView,
-    TextInput,
-    Platform,
-    StatusBar,
-    ScrollView,
-    Image,
-    Dimensions
-} from "react-native";
+import { View, Text, FlatList, Image, TouchableWithoutFeedback } from "react-native";
+
+import { Container, Header, Title, Content, Button, Icon, Left, Right, Body, Text as NBText, 
+  ListItem, List } from "native-base";
+
+const datas = [
+  {
+    route: "membership",
+    text: "Points"
+  },
+  {
+    route: "rewards",
+    text: "Rewards"
+  },
+  {
+    route: "scheduled",
+    text: "Scheduled"
+  },
+  {
+    route: "subscriptions",
+    text: "Subscriptions"
+  },
+  {
+    route: "savedPlaces",
+    text: "Saves Places"
+  },
+  {
+    route: "helpCenter",
+    text: "Help Center"
+  },
+  {
+    route: "emergency",
+    text: "Emergency"
+  },
+  {
+    route: "settings",
+    text: "Settings"
+  },
+  {
+    route: "shareFeedback",
+    text: "Share Feedback"
+  },
+  {
+    route: "driveWithGrab",
+    text: "Drive With Grab"
+  }
+];
 
 export default class App extends Component {
-  
   render() {
     return (
-      <View>
-        <Text>Account Screen</Text>
-      </View>
+      <Container>
+        <Content>
+          <View style={{
+            flex: 1,
+            flexDirection: "column",
+          }}>
+            <TouchableWithoutFeedback onPress={() => alert('on press profile')}>
+              <View style={{
+                flexDirection: "row",
+              }}>
+                <View>
+                  <Image source={{ uri: "https://cdn.dribbble.com/users/1236180/screenshots/4440250/shot.jpg" }} 
+                    style={{ width: 130, height: 130}} />
+                </View>
+                <View style={{
+                  flex: 1,
+                  flexDirection: "column",
+                  justifyContent: "center",
+                }}>
+                  <NBText>Jon Snow</NBText>
+                  <Text>Edit Profile ></Text>
+                </View>
+              </View>
+            </TouchableWithoutFeedback>
+            <View>
+              <FlatList
+                data={datas}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item, index }) => (
+                  // <ListItem button onPress={() => this.props.navigation.navigate(item.route)} >
+                  <ListItem button onPress={() => alert('navigate to:: ' + item.route)} >
+                    <Left>
+                      <NBText>{item.text}</NBText>
+                    </Left>
+                    <Right>
+                      <Icon name="arrow-forward" />
+                    </Right>
+                  </ListItem>
+                )}
+              />
+            </View>
+          </View>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 50,
-    marginBottom: 20,
-    backgroundColor: "#fefffe"
-  },
-  home: {
-    paddingHorizontal: 20
-  }
-});
